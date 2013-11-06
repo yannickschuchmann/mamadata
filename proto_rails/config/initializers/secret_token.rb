8963e4fe75ad0e682df3552eb24171fe9fe05c76
Michael Lazarski <michael@lazarski.me>
2013-11-06 10:54:01 +0100

    first commit

---
+++ proto_rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProtoRails::Application.config.secret_key_base = '91474176f6a4446605cd783bf6ee0c83084aaa3de5636ba1b8f0370ede5ce7623008ed3f1bf1734467a91c5502193a5e0cea5cb78ab5af281d0d9c67cd46f81f'
---
+++ proto_rails/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProtoRails::Application.config.session_store :cookie_store, key: '_proto_rails_session'
