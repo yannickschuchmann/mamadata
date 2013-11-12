4aec064dd4f492b8f83944e88f547c975feb35ec
Dariusch Ochlast <Dariusch.Ochlast@googlemail.com>
2013-11-13 00:44:13 +0100

    added some beneficiary seed data, command rake db:seed

--- proto_rails/db/seeds.rb
+++ proto_rails/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Beneficiary.create({"name"=>"Dariusch Ochlast", "fathers_name"=>"Ochlast", "gender"=>"male", "date_of_birth"=>"1991-01-16", "place_of_birth"=> "Krapkowice", "native_place" => "Mainz", "house_name"=>"Paul Sudeck Haus", "house_number"=>"134", "street_name"=> "Wiesendamm", "city" => "Hamburg", "pin_code"=> 22303, "surname"=>"Ochlast", "narrative_text"=> "Hallo ich bin Dariusch und hier soll ein über 255 Zeichen langer Text rein!", "school_name"=>"HAW Hamburg", "school_class"=> "university", "health_condition"=>"normal" })
+Beneficiary.create({"name"=>"Test User", "fathers_name"=>"Heidewitzka", "gender"=>"female", "date_of_birth"=>"2013-11-12", "place_of_birth"=> "Hamburg", "native_place" => "Hamburg", "house_name"=>"Paul Sudeck Haus", "house_number"=>"134", "street_name"=> "Wiesendamm", "city" => "Hamburg", "pin_code"=> 22303, "surname"=>"Heidewitzka", "narrative_text"=> "Hallo ich bin Heidewitzka ich wurde soeben von Dariusch erfunden und soll ein langer Text sein. Dies gelingt mit leider nicht so ganz also werde ich hier einfach weiterschwafeln =)", "school_name"=>"HAW Hamburg", "school_class"=> "university", "health_condition"=>"mentally retarded" })
