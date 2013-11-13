347db0f60493fe97220454526943801db3953b08
Michael Lazarski <michael@lazarski.me>
2013-11-13 18:18:02 +0100

    new rails4 project

---
+++ mamadata_rails4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MamadataRails4::Application.config.secret_key_base = '0d86e027aeaa35a936a6ab802115f3229ca166eb63d05187e7d247a8fd8788fd8e0af6dceea2eff969f640a2357fa8d75d015edcc6bcd26b8f208f44245a96cf'
---
+++ mamadata_rails4/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MamadataRails4::Application.config.session_store :cookie_store, key: '_mamadata_rails4_session'
