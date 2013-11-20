9f9710be1720b14c2381518a19424d1d20f30b36
Dariusch Ochlast <Dariusch.Ochlast@googlemail.com>
2013-11-20 23:09:04 +0100

    cleanup

--- mamadata_rails4/db/schema.rb
+++ mamadata_rails4/db/schema.rb
@@ -119,10 +119,4 @@ ActiveRecord::Schema.define(version: 20131120200836) do
     t.datetime "updated_at"
   end
 
-  create_table "tests", force: true do |t|
-    t.boolean  "asdf"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
