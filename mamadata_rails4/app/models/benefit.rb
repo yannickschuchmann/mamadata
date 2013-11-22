ff96c43a501be54c146aeba647d5955bc14cd6af
Danu <jonasdanu.bekas@haw-hamburg.de>
2013-11-22 23:09:57 +0100

    benfit and program now have add and remove methods

--- mamadata_rails4/app/models/benefit.rb
+++ mamadata_rails4/app/models/benefit.rb
@@ -1,4 +1,13 @@
 class Benefit < ActiveRecord::Base
 	has_many :program_benefit_relationships
 	has_many :programs, through: :program_benefit_relationships 
+
+
+	def add_program(program)
+		program_benefit_relationships.create!(program_id: program.id)
+	end
+
+	def remove_program(program)
+		program_benefit_relationships.find_by(program_id: program.id).destroy!
+	end
 end
--- mamadata_rails4/app/models/program_benefit_relationship.rb
+++ mamadata_rails4/app/models/program_benefit_relationship.rb
@@ -1,4 +1,6 @@
 class ProgramBenefitRelationship < ActiveRecord::Base
 	belongs_to :program
 	belongs_to :benefit
+	validates :program_id, presence: true
+	validates :benefit_id, presence: true
 end
