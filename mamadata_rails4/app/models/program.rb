25e5b13a997388873559494e92b9818bd18b77d1
Dariusch Ochlast <Dariusch.Ochlast@googlemail.com>
2013-11-28 00:00:37 +0100

    Beneficiary belongs now to program

--- mamadata_rails4/app/models/program.rb
+++ mamadata_rails4/app/models/program.rb
@@ -1,5 +1,6 @@
 class Program < ActiveRecord::Base
 	has_many :program_benefit_relationships
 	has_many :benefits, through: :program_benefit_relationships
+	has_many :beneficiaries
 	accepts_nested_attributes_for :benefits
 end
