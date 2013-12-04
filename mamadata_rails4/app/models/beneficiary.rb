10225f992d568622edaf3eb118c0df31c7c374dd
Michael Lazarski <michael@lazarski.me>
2013-12-04 14:30:34 +0100

    godfather now works in beneficiarie

--- mamadata_rails4/app/models/beneficiary.rb
+++ mamadata_rails4/app/models/beneficiary.rb
@@ -1,6 +1,6 @@
 class Beneficiary < ActiveRecord::Base
   has_many :members
   has_many :programs, through: :members
-  belongs_to :godfather
+  belongs_to :godfather, :class_name => "Supporter"
   has_many :benefit_incidents
 end
--- mamadata_rails4/app/models/supporter.rb
+++ mamadata_rails4/app/models/supporter.rb
@@ -1,2 +1,7 @@
 class Supporter < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :city, presence: true
+	validates :country, presence: true
+	validates :zipcode, presence: true
 end
