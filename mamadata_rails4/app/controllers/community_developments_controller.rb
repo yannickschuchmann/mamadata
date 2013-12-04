674ad4555e3d6f3fda7734d6230eccd9cd9fa3da
Michael Lazarski <michael@lazarski.me>
2013-12-05 00:36:10 +0100

    fixed comDev save problems

--- mamadata_rails4/app/controllers/community_developments_controller.rb
+++ mamadata_rails4/app/controllers/community_developments_controller.rb
@@ -40,6 +40,10 @@ class CommunityDevelopmentsController < ApplicationController
   # PATCH/PUT /community_developments/1
   # PATCH/PUT /community_developments/1.json
   def update
+    puts "33333333333333333333333333333333333"
+    community_development_params["family_skilled_in_art"].reject!(&:empty?)
+    puts community_development_params["family_skilled_in_art"].class
+    puts community_development_params["illness_treatment"].reject!(&:empty?)
     respond_to do |format|
       if @community_development.update(community_development_params)
         format.html { redirect_to @community_development, notice: 'Community development was successfully updated.' }
@@ -69,6 +73,6 @@ class CommunityDevelopmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def community_development_params
-      params.require(:community_development).permit(:head_of_household, :type_of_family, :residental_status, :house_posession, :house_type, :electricity, :water_from, :toilet_facility, :rain_harvesting, :want_rain_harvesting, :earning_members_of_family, :source_of_income, :land_details, :any_savings, :mode_of_savings, :debts, :reason_for_debt, :family_members_addicted_to, :family_members_addicted_who, :family_disabled, :family_type_of_handicap, :family_chronic_deseases, :family_chronic_deseases_which, :family_children_vaccined, :family_knowledge_of_family_planning, :family_undergone_family_planning, :home_remedies_for_illnesses, :illness_treatment, :family_school_dropout, :family_dropout_reason_for, :family_dropout_present_condition, :family_skilled_in_art)
+      params.require(:community_development).permit(:head_of_household_id, :type_of_family, :residental_status, :house_posession, :house_type, :electricity, :water_from, :toilet_facility, :rain_harvesting, :want_rain_harvesting, :earning_members_of_family, :source_of_income, :land_details, :any_savings, :mode_of_savings, :debts, :reason_for_debt, :family_members_addicted_to, :family_members_addicted_who, :family_disabled, :family_type_of_handicap, :family_chronic_deseases, :family_chronic_deseases_which, :family_children_vaccined, :family_knowledge_of_family_planning, :family_undergone_family_planning, :home_remedies_for_illnesses, :family_school_dropout, :family_dropout_reason_for, :family_dropout_present_condition, {:family_skilled_in_art => []},{:illness_treatment => []})
     end
 end
