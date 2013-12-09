db8b2fb0f2c6b97eeab0001c750c4bead9868212
Michael Lazarski <michael@lazarski.me>
2013-12-09 22:12:39 +0100

    counting now the people

--- mamadata_rails4/app/assets/javascripts/families.js
+++ mamadata_rails4/app/assets/javascripts/families.js
@@ -1,8 +1,10 @@
+peopleNumber = 1;
 $("#addpeople").click(function(){
-    $.ajax({
-        url: "/families/addpeople.js",
-        type: "GET",
-        success: function(result){ console.log("fine!"); },
-        error: function(){ alert('Error occured'); }
-    });
+    $("#people").append('<h1>Person'+peopleNumber+'</h1><div class="field">'+
+                        '<div class="span3"><label for="name">Name</label></div>'+
+                        '<div class="span9"><input id="name" name="person[name]" type="text"></div></div>'+
+                        '<div class="field"><div class="span3"><label for="person_Father\'s Name">Father\'s name</label></div>'+
+                        '<div class="span9"><input id="person_fathers_name" name="person[fathers_name]" type="text"></div></div>'
+    );
+    peopleNumber = peopleNumber + 1;
 });
