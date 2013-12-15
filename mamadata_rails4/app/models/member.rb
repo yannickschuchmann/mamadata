9cf0b2d5565393586faaeae19130f401906f8717
Danu Bekas <jonasdanu.bekas@haw-hamburg.de>
2013-12-15 21:56:28 +0100

    fixed table change

--- mamadata_rails4/app/models/member.rb
+++
@@ -1,6 +0,0 @@
-class Member < ActiveRecord::Base
-	belongs_to :program
-	belongs_to :beneficiary
-	# validates :program_id, presence: true
-	# validates :person_id, presence: true
-end
--- mamadata_rails4/db/migrate/20131215202820_drop_members.rb
+++
@@ -1,5 +0,0 @@
-class DropMembers < ActiveRecord::Migration
-  def change
-  	drop_table :members
-  end
-end
