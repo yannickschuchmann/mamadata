da43529a857c03e15effae7c1990f0c32c716db9
Michael Lazarski <michael@lazarski.me>
2013-12-17 15:11:09 +0100

    merged branches

--- mamadata_rails4/db/schema.rb
+++ mamadata_rails4/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215202820) do
+ActiveRecord::Schema.define(version: 20131215202635) do
 
   create_table "beneficiaries", force: true do |t|
     t.string   "school_name"
