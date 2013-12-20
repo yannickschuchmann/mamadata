31eeb7fbf91b65eef3c992861b11c4a718ac803d
Danu Bekas <jonasdanu.bekas@haw-hamburg.de>
2013-12-20 13:54:08 +0100

    changed

--- mamadata_rails4/app/models/benefit_incident.rb
+++ mamadata_rails4/app/models/benefit_incident.rb
@@ -2,7 +2,6 @@ class BenefitIncident < ActiveRecord::Base
 	belongs_to :person
 	belongs_to :program
 	belongs_to :benefit
-	belongs_to :beneficiary
 	validates :person_id, presence: true
 	validates :program_id, presence: true
 	validates :benefit_id, presence: true
