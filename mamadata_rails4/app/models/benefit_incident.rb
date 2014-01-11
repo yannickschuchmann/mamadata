5a4c01de155ea808180b64a166fc47e810b1b8eb
Danu Bekas <jonasdanu.bekas@haw-hamburg.de>
2014-01-11 18:21:54 +0100

    added date_granted

--- mamadata_rails4/app/models/benefit_incident.rb
+++ mamadata_rails4/app/models/benefit_incident.rb
@@ -6,12 +6,17 @@ class BenefitIncident < ActiveRecord::Base
 	validates :program_id, presence: true
 	validates :benefit_id, presence: true
 	before_save :set_default_status
+	before_save :set_date_granted
 
 	protected
 		
 		def set_default_status
 			self.status = false unless self.status
 			self.date_granted = nil unless self.date_granted
+		end
+
+		def set_date_granted
+			self.date_granted = DateTime.now if self.status == true
 		end
 
 
