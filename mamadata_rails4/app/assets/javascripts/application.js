5ec58f53c58d4b3fc44cd13d535298ee6b052ffc
Dariusch Ochlast <dariusch.ochlast@gmail.com>
2014-02-06 16:10:47 +0100

    javascript moved to asset

--- mamadata_rails4/app/assets/javascripts/application.js
+++ mamadata_rails4/app/assets/javascripts/application.js
@@ -24,6 +24,33 @@ $(function(){
 //= require turbolinks
 //= require jquery.tablesorter.min
 $(function(){
-
 	$(document).foundation();
+});
+
+// Off-Canvas function
+$(function () {
+		$sideWidth = $('#side').outerWidth();
+		$('#content').css("margin-left", ($sideWidth + 10)+"px");
+	});
+
+var canvas_toggle 	= $('#canvas-toggle'),
+	off_canvas 		= $('#off-side'),
+	side 			= $('#side'),
+	wrapper			= $('#wrapper');
+
+canvas_toggle.click( function() {
+	if (off_canvas.hasClass("is_active")) {
+		off_canvas.removeClass("is_active");
+		off_canvas.css("left", "-150px");
+		wrapper.css("margin-left", "0px")
+		side.css("left", "0px");
+	}else {
+		off_canvas.addClass("is_active");
+		off_canvas.css("left", "0px");
+		wrapper.css("margin-left", "160px")
+		side.css("left", "150px");
+	};
+});
+$(document).ready(function(){
+	$(".tablesorter").tablesorter();
 });
