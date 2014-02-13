58ed438a143aeae2cb4fbe706b1c3fd076351005
Danu Bekas <jonasdanu.bekas@haw-hamburg.de>
2014-02-13 18:18:26 +0100

    refactoring

--- mamadata_rails4/app/assets/javascripts/people.js
+++ mamadata_rails4/app/assets/javascripts/people.js
@@ -1,65 +1,63 @@
-
+/* update incidents via ajax function */
 var updateIncidentList = function() {
 	$.ajax({
-    url: "/benefit_incidents/list/"+ personId,
-    type: "GET",
-    data: {status: $('#incident_status option:selected').val(), list_date: $('#benefit_incident_created_at').val()}
-    })
+        url: "/benefit_incidents/list/"+ personId,
+        type: "GET",
+        data: {status: $('#incident_status option:selected').val(), list_date: $('#benefit_incident_created_at').val()}
+    });
     $('#calculated_amount').val('');
     $('#calculated_amount').hide();
 };
 
+
+/* update incidents when status of select box changes */
 $('#incident_status').change(function(){
 	updateIncidentList();
-
-
 });
 
+
+/* update checked incidents with status true via ajax */
 $('#setgrantedbtn').on('click', function(e){
 	e.preventDefault();
-$(".chk").each(function() {
-	if($(this).is(':checked')){
-	  $.ajax({
-    url: "/benefit_incidents/" + $(this).val(),
-    type: "PATCH",
-    data: {benefit_incident: {status: true}},
-    dataType: "json"
-  })
-	}
-});
+    $(".chk").each(function() {
+	   if($(this).is(':checked')){
+	       $.ajax({
+                url: "/benefit_incidents/" + $(this).val(),
+                type: "PATCH",
+                data: {benefit_incident: {status: true}},
+                dataType: "json"
+            })
+	   }
+    });
 	updateIncidentList();
 });
 
 
-
-
+/* send array of ids to calculate amount on via ajax */
 $('#calculatebtn').on('click', function(e){
 	e.preventDefault();
 	var ids = new Array();
-$(".chk").each(function() {
-	if($(this).is(':checked')){
-		ids.push($(this).val());
-	}
-});
+    $(".chk").each(function() {
+	   if($(this).is(':checked')){
+		  ids.push($(this).val());
+	   }
+    });
 $("#calculated_amount").show();
 $.ajax({
     url: "/benefit_incidents/test/calculated",
     type: "GET",
     data: {array: ids},
-  })
+  });
 });
 
 
-
-
+/* update incidents when dateFilter applied */
 $('#benefit_incident_created_at').change(function(){
-updateIncidentList();
-
+    updateIncidentList();
 });
 
 
-
-
+/* initialize datepicker */
 $('#benefit_incident_created_at').datepicker({
     dateFormat:'yy-mm-dd',
     duration: 'normal',
@@ -69,9 +67,10 @@ $('#benefit_incident_created_at').datepicker({
     autoSize: true,
     showButtonPanel: true,
     inline: true
-
 });
 
+
+/* checkox shows datepicker */
 $('#filter_date').change( function(){
 	if($(this).is(':checked')){
 		$('#benefit_incident_created_at').show();
