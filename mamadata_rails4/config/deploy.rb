4c4ae879e201053a9c7085ce6d87da0ddd9ace04
Michael Lazarski <michael@lazarski.me>
2014-02-15 13:38:04 +0100

    deployment fix

--- mamadata_rails4/config/deploy.rb
+++ mamadata_rails4/config/deploy.rb
@@ -49,9 +49,10 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'cache:clear'
+        execute :rake, 'assets:precompile'
+      end
     end
   end
 
