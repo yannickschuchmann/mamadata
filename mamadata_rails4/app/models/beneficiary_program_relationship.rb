bb64ff75fd7fe3fce55cf504a78a10f257419bae
Danu Bekas <jonasdanu.bekas@haw-hamburg.de>
2014-02-17 22:08:17 +0100

    test

--- mamadata_rails4/app/models/beneficiary_program_relationship.rb
+++ mamadata_rails4/app/models/beneficiary_program_relationship.rb
@@ -3,7 +3,11 @@ class BeneficiaryProgramRelationship < ActiveRecord::Base
 	belongs_to :person
 	# before_save :set_active_flag
 
-
+  # before_destroy { |record|
+  #   self.is_active = false	
+  #   self.save!
+  #   false
+  # }
 	# private
 	# 	def set_active_flag
 	# 		puts "blubb"
