c04e7434f9cf1c4f0792e72e3fe7cf59ba17ee70
Danu Bekas <jonasdanu.bekas@haw-hamburg.de>
2014-02-18 21:51:27 +0100

    updated view for godfather history

--- mamadata_rails4/app/controllers/person_godfather_files_controller.rb
+++ mamadata_rails4/app/controllers/person_godfather_files_controller.rb
@@ -1,25 +1,29 @@
 class PersonGodfatherFilesController < ApplicationController
   before_action :set_person_godfather_file, only: [:show, :edit, :update, :destroy, :getfile]
-	
+
   def getfile
     send_file @person_godfather_file.file.path, :type => @person_godfather_file.file_content_type
   end
 
-	def new
-		@person_godfather_file = PersonGodfatherFile.new(godfather_person_id: params[:id])
-	end
+  def new
+    @person_godfather_file = PersonGodfatherFile.new(godfather_person_id: params[:id])
+  end
 
 
-def create
-  @person_godfather_file = PersonGodfatherFile.create( person_godfather_file_params )
-   respond_to do |format|
+  def create
+    @person_godfather_file = PersonGodfatherFile.create( person_godfather_file_params )
+    respond_to do |format|
       if @person_godfather_file.save
-        format.html { redirect_to root_path, notice: 'File upload successfull' }
+        format.html { redirect_to person_path(@person_godfather_file.godfather_person.person_id),
+           notice: 'File successfully uploaded' }
+        else
+          format.html { redirect_to person_path(@person_godfather_file.godfather_person.person_id),
+             notice: 'Something went wrong' }
+          end
+        end
       end
-    end
-end
 
-private
+      private
 
 # Use strong_parameters for attribute whitelisting
 # Be sure to update your create() and update() controller methods.
@@ -28,7 +32,7 @@ def person_godfather_file_params
   params.require(:person_godfather_file).permit(:file, :godfather_person_id)
 end
 
-  def set_person_godfather_file
-    @person_godfather_file = PersonGodfatherFile.find(params[:id])
-  end
+def set_person_godfather_file
+  @person_godfather_file = PersonGodfatherFile.find(params[:id])
+end
 end
