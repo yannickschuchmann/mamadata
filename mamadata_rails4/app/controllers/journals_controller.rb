0ea1c969b8d9408a0627aff08b67b25869030988
Dariusch Ochlast <dariusch.ochlast@gmail.com>
2014-02-19 19:33:38 +0530

    edited journal link

--- mamadata_rails4/app/controllers/journals_controller.rb
+++ mamadata_rails4/app/controllers/journals_controller.rb
@@ -2,12 +2,12 @@ class JournalsController < ApplicationController
 def create
     @person = Person.find(params[:person_id])
     @journal = @person.journals.create(params[:journal].permit(:text, :date))
-    redirect_to person_path(@person)
+    redirect_to person_path(@person)+"#panel2-5"
   end 
   def destroy
     @person = Person.find(params[:person_id])
     @journal = @post.journals.find(params[:id])
     @journal.destroy
-    redirect_to journal_path(@post)
+    redirect_to journal_path(@post)+"#panel2-5"
   end
 end
