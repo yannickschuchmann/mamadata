8fda52a304f5e366d069eabe8f6e5cf2996b24a6
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2014-02-20 20:57:26 +0530

    removed unnecessary person_id from comdev

--- mamadata_rails4/db/schema.rb
+++ mamadata_rails4/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220132112) do
+ActiveRecord::Schema.define(version: 20140220152246) do
 
   create_table "beneficiary_program_relationships", force: true do |t|
     t.integer  "program_id"
