0e50071e69f7d32da19540da648ade4e9b26a389
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2014-02-21 15:37:25 +0530

    things

--- mamadata_rails4/config/application.rb
+++ mamadata_rails4/config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'monetize/core_extensions'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
