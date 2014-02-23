b2956dde70f2fb81af96f2d40c6bd3332efb0ace
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2014-02-23 15:06:39 +0530

    validates file upload school class

--- mamadata_rails4/app/models/school_class.rb
+++ mamadata_rails4/app/models/school_class.rb
@@ -2,4 +2,11 @@ class SchoolClass < ActiveRecord::Base
   has_attached_file :document
   #validates_attachment :document, content_type: { content_type: "application/pdf" }
   belongs_to :school
+  validate :reset_file
+
+  private
+
+  def reset_file
+    self.document_file_name = self.changed_attributes["document_file_name"] unless self.changed_attributes["document_file_name"].nil? or self.document_file_name.present?
+  end
 end
