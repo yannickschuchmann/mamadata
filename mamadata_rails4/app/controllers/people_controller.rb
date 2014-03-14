9541613f490a9487a824814e8d3fe0750f3fa0dc
Danu Bekas <jonasdanu.bekas@haw-hamburg.de>
2014-03-14 18:24:37 +0100

    removed old xls file reports

--- mamadata_rails4/app/controllers/people_controller.rb
+++ mamadata_rails4/app/controllers/people_controller.rb
@@ -6,9 +6,6 @@ class PeopleController < ApplicationController
   def xlsreport
   	 @people = Person.all
   	 render :xlsx => "xlsreport", :filename => "beneficiary_report#{DateTime.now}.xlsx"
-  	 # respond_to do |format|
-  	 # 	format.xls
-  	 # end
 	end
 
   def index
