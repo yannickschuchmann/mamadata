b3e0c130f33eb659c1e3045d20c3fcfc8078fb7e
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2014-04-29 21:54:22 +0200

    fixed default avatar gif->png

--- mamadata_rails4/app/models/person.rb
+++ mamadata_rails4/app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
   acts_as_xlsx
 
-  has_attached_file :avatar, :styles => {:medium => "300x300>", :small => "160>", :thumb => "100x100>"}, :default_url => "/images/missing_:style.gif"
+  has_attached_file :avatar, :styles => {:medium => "300x300>", :small => "160>", :thumb => "100x100>"}, :default_url => "/images/missing_:style.png"
   belongs_to :role
   belongs_to :family
   belongs_to :user, foreign_key: 'created_by'
