4c470bb0499d124890c92300b86c13fa886336be
Danu <danu.bekas@posteo.de>
2014-08-30 12:54:12 +0200

    view refactorings

--- mamadata_rails4/app/models/godfather_person.rb
+++ mamadata_rails4/app/models/godfather_person.rb
@@ -1,6 +1,6 @@
 class GodfatherPerson < ActiveRecord::Base
 	acts_as_paranoid
-	belongs_to :person
+	belongs_to :person, touch: true
 	belongs_to :godfather, class_name: "Supporter"
 	has_many :person_godfather_files
 	accepts_nested_attributes_for :person_godfather_files
