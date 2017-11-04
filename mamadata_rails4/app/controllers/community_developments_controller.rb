3c7fc72046fac959e1a8ef8025f4166d0d97adbe
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2017-11-04 18:18:11 +0100

    change code style

--- mamadata_rails4/app/controllers/community_developments_controller.rb
+++ mamadata_rails4/app/controllers/community_developments_controller.rb
@@ -1,5 +1,7 @@
 class CommunityDevelopmentsController < ApplicationController
-	before_action :set_community_development, only: [:edit, :update, :destroy]
+	before_action :set_community_development, only: [:edit,
+:update,
+:destroy]
 
 	# PATCH/PUT /community_developments/1
 	# PATCH/PUT /community_developments/1.json
@@ -36,6 +38,44 @@ class CommunityDevelopmentsController < ApplicationController
 
 		# Never trust parameters from the scary internet, only allow the white list through.
 		def community_development_params
-			params.require(:community_development).permit(:id, :family, :type_of_family, :residental_status, :house_possession, :house_type, :electricity, :water_from, :toilet_facility, :rain_harvesting, :want_rain_harvesting, :earning_members_of_family, :source_of_income, :land_details, :any_savings, :mode_of_savings, :debts, :reason_for_debt, :family_members_addicted_to, :family_members_addicted_who, :family_disabled, :family_type_of_handicap, :family_chronic_deseases, :family_chronic_deseases_which, :family_children_vaccined, :family_knowledge_of_family_planning, :family_undergone_family_planning, :home_remedies_for_illnesses, :family_school_dropout, :family_dropout_reason_for, :family_dropout_present_condition, {:family_skilled_in_art => []},{:illness_treatment => []})
+			params.require(:community_development).permit(
+				:id,
+				:family,
+				:type_of_family,
+				:residental_status,
+				:house_possession,
+				:house_type,
+				:electricity,
+				:water_from,
+				:toilet_facility,
+				:rain_harvesting,
+				:want_rain_harvesting,
+				:earning_members_of_family,
+				:source_of_income,
+				:land_details,
+				:any_savings,
+				:mode_of_savings,
+				:debts,
+				:reason_for_debt,
+				:family_members_addicted_to,
+				:family_members_addicted_who,
+				:family_disabled,
+				:family_type_of_handicap,
+				:family_chronic_deseases,
+				:family_chronic_deseases_which,
+				:family_children_vaccined,
+				:family_knowledge_of_family_planning,
+				:family_undergone_family_planning,
+				:home_remedies_for_illnesses,
+				:family_school_dropout,
+				:family_dropout_reason_for,
+				:family_dropout_present_condition,
+				{
+					:family_skilled_in_art => []
+				},
+				{
+					:illness_treatment => []
+				}
+			)
 		end
 	end
